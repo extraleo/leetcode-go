0f273d7ff107574f1a6d290f843bb3e56d910351
leih <ray.leiheng@qq.com>
2020-11-06 17:54:24 +0800

    update-21

--- leetcode/21.merge-two-sorted-lists/21.merge-two-sorted-lists.go
+++ leetcode/21.merge-two-sorted-lists/21.merge-two-sorted-lists.go
@@ -14,9 +14,25 @@ type ListNode = structures.ListNode
 
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-
+	return mergeTwoListsRecursion(l1,l2)
 }
 
+
+func mergeTwoListsRecursion(l1,l2 *ListNode) *ListNode{
+	if l1 == nil{
+		return l2
+	}
+	if l2 == nil{
+		return l1
+	}
+
+	if l1.Val < l2.Val{
+		l1.Next = mergeTwoListsRecursion(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoListsRecursion(l2.Next, l1)
+	return l2
+}
 
 // using dummy node to create list
 // seems not very fast:
