c115f05e776ad694337e7b7b04d3e277a669e7db
leih <ray.leiheng@qq.com>
2020-11-06 17:39:35 +0800

    21

--- leetcode/21.merge-two-sorted-lists/21.merge-two-sorted-lists.go
+++ leetcode/21.merge-two-sorted-lists/21.merge-two-sorted-lists.go
@@ -12,13 +12,42 @@ import "github.com/extraleo/alg/structures"
 
 type ListNode = structures.ListNode
 
-// using dummy node to create list
+
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	dummy := &ListNode{0, &ListNode{}}
-	for l1.Next != nil && l2.Next != nil {
-		
+
+}
+
+
+// using dummy node to create list
+// seems not very fast:
+// Your runtime beats 58.55% of golang submissions
+// Your memory usage beats 62.46% of golang submissions
+func mergeTwoListIteration(l1,l2 *ListNode) *ListNode{
+		if l1 == nil{
+		return l2
 	}
 
+	if l2 == nil{
+		return l1
+	}
+	dummy := &ListNode{0, &ListNode{}}
+	head := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			head.Next = l1
+			l1 = l1.Next
+		}else{
+			head.Next = l2
+			l2 = l2.Next
+		}
+		head=head.Next
+	}
+	if l1 != nil {
+		head.Next = l1
+	}
+	if l2 != nil{
+		head.Next =l2
+	}
 	return dummy.Next
 }
 
